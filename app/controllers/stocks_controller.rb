ffa80dbf47950a5507e61731994a9f18209412ca
lawyer-omer <av.omeraydin29@gmail.com>
2022-06-08 17:27:38 +0300

    added stock table

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,7 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   # GET /stocks or /stocks.json
   def index
@@ -55,6 +57,11 @@ class StocksController < ApplicationController
       format.html { redirect_to stocks_url, notice: "Stock was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @ticker = current_user.stocks.find_by(id: params[ :id ])
+    redirect_to stocks_path, notice: "Not Authorize to edit this stock" if @ticker.nil?
   end
 
   private
